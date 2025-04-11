64ff3aa9060e42c6f1b46f5f3c23915c9db7893f
jithin <hello@jithinsha.in>
2025-04-11 12:10:27 +0530

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,5 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
-  vite: {
-    server: {
-      fs: {
-        allow: [
-          './',
-          '../',
-          'C:/Users/Jithin/Downloads/project-notes/project'  // 👈 add this
-        ]
-      }
-    }
-  }
+  modules: ['@nuxtjs/tailwindcss']
 })
